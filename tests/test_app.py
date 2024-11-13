afaf18fc2100a6aefe2a5b7b5e93da8fa6337c06
Aya Al Baba <ayababa12@outlook.com>
2024-11-13 11:34:29 +0200

    fixed pytest

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,13 @@
 import unittest
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import greet
 
 class TestApp(unittest.TestCase):
     def test_greet(self):
-        self.assertEqual(greet("World"), "Hello, World from FirstName LastName!")
+        self.assertEqual(greet("World from Aya Al Baba"), "Hello, World from Aya Al Baba!")
 if __name__ == "__main__":
     unittest.main()
